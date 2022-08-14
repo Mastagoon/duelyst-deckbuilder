4fdf5f1c398329a660ce138f1fd535f181fb3b48
Mastagoon <inzilagiway@gmail.com>
2022-08-14 11:20:42 +0300

    imported deck data from duelyst-share

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gradient-primary-from": "rgba(19, 14, 77, 1)",
+        "gradient-primray-to": "rgba(141, 7, 104, 0)",
+        "primary-dark-blue": "rgba(9, 5, 52, 1)",
+        "secondary-dark-blue": "rgba(41, 16, 72, 1)",
+        "primary-light-blue": "rgba(61, 174, 217, 1)",
+        "primary-light-purple": "rgba(89, 81, 188, 1)",
+        "secondary-light-purple": "rgba(89, 81, 188, 0.35)",
+      },
+    },
   },
   plugins: [],
-};
+}
