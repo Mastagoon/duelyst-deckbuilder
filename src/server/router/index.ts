0bc125530ff48db48ebe01389d0f68ed1a2997e1
Mastagoon <inzilagiway@gmail.com>
2022-09-01 19:06:07 +0300

    Build error

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,13 +3,11 @@ import { createRouter } from "./context"
 import superjson from "superjson"
 import { exampleRouter } from "./example"
 import { deckRouter } from "./deck"
-import { imageRouter } from "./image"
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("example", exampleRouter)
   .merge("deck", deckRouter)
-  .merge("images", imageRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
